001ef16f4c461dbd96b0e4bfc74fee1bb0fb2bb3
Prescott Rowe <prescottrowe@gmail.com>
2020-05-12 11:23:11 -0700

    VirusTotal Free lookup
    Pass the script a csv file of URLs to look up on VT using the free api that only allows 4 lookups a minute.

--- VTclient.py
+++ VTclient.py
@@ -3,17 +3,17 @@ import urllib2
 import time
 import csv
 
-api_token = ''
-api_url_base = 'https://www.virustotal.com/vtapi/v2/url/report?apikey='+ api_token
-
-with open('covid_URLs_VT_lookup.csv') as csv_file:
+api_token = ''#get a free token
+api_url_base = 'https://www.virustotal.com/vtapi/v2/url/report?apikey='+ api_token #line for url lookups
+#point it as a csv file. file should have a header for each colum that is used for reference.
+with open('lookup.csv') as csv_file:
     urlReader = csv.reader(csv_file, delimiter=',')
     for row in urlReader:
         searchUrl = ', '.join(row)
-        get_report_url = api_url_base + '&resource='+ searchUrl
+        get_report_url = api_url_base + '&resource='+ searchUrl #build the full REST request
         request = urllib2.Request(get_report_url)
-        response = urllib2.urlopen(request)
-        data = json.load(response)
+        response = urllib2.urlopen(request) #send request and get response
+        data = json.load(response) #parse
         try:
             print (searchUrl +" " + str(data["positives"]) + "/" + str(data["total"]))
         except:
